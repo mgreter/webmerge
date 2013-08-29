747c9a8b6f96b81d8bc2c93be9476c9299d4d127
mgreter <marcel.greter@ocbnet.ch>
2013-08-29 10:54:20 +0200

    Added install/update script for google closure compiler

---
+++ scripts/closure-compiler/update.bat
@@ -0,0 +1,7 @@
+@echo off
+
+REM You need to have the unix utils installed
+REM http://unxutils.sourceforge.net/UnxUtils.zip
+
+wget "http://dl.google.com/closure-compiler/compiler-latest.zip"
+unzip -o "compiler-latest.zip" && del "compiler-latest.zip"
---
+++ scripts/closure-compiler/update.sh
@@ -0,0 +1,2 @@
+wget "http://dl.google.com/closure-compiler/compiler-latest.zip"
+unzip -o "compiler-latest.zip" && rm "compiler-latest.zip"
