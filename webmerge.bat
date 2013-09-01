34182b152c461da5546b065a94ef63fd18b759b1
Marcel Greter <marcel.greter@ocbnet.ch>
2013-09-01 19:38:53 +0200

    Added more options for local perl and graphicsmagick installation

--- webmerge.bat
+++ webmerge.bat
@@ -1,10 +1,34 @@
 @echo off
 
-IF NOT EXIST perl GOTO noLocalPerl
+SETLOCAL
+
+SET GMPATH=..\gm
+SET IMPATH=..\im
+SET PERLPATH=..\perl
+
+IF NOT EXIST %GMPATH% GOTO noLocalGM
+
+REM add local gm to global path
+SET PATH=%PATH%;%GMPATH%
+
+:noLocalGM
+
+IF NOT EXIST %IMPATH% GOTO noLocalIM
+
+REM add local im to global path
+SET PATH=%PATH%;%IMPATH%
+
+:noLocalIM
+
+IF NOT EXIST %PERLPATH% GOTO noLocalPerl
 
 REM add local perl to global path
-set PATH=%PATH%;perl\c\bin;perl\perl\site\bin;perl\perl\bin;
+SET PATH=%PATH%;%PERLPATH%\c\bin
+SET PATH=%PATH%;%PERLPATH%\perl\site\bin
+SET PATH=%PATH%;%PERLPATH%\perl\bin
 
 :noLocalPerl
 
 perl scripts\webmerge.pl %*
+
+ENDLOCAL
