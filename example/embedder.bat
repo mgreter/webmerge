d6c8f838922fe1feac90176a37a9da342e1c7c85
Marcel Greter <marcel.greter@ocbnet.ch>
2013-12-28 06:44:37 +0100

    Adds webserver to some examples

--- example/embedder.bat
+++ example/embedder.bat
@@ -1,3 +1,9 @@
 @echo off
 
-"%~dp0\..\webmerge.bat" -f "%~dp0\conf\embedder.conf.xml" %*
+call "%~dp0\..\webmerge.bat" -f "%~dp0\conf\embedder.conf.xml" %*
+
+start "" "http://localhost:8080"
+
+call "%~dp0\..\webmerge.bat" -f "%~dp0\conf\embedder.conf.xml" %* --webserver
+
+REM call "%~dp0\..\webmerge.bat" -f "%~dp0\conf\embedder.conf.xml" %* --watchdog --dbg
--- example/sprites.bat
+++ example/sprites.bat
@@ -1,3 +1,10 @@
 @echo off
 
-"%~dp0\..\webmerge.bat" -f "%~dp0\conf\sprites.conf.xml" -o -png fam hires %*
+call "%~dp0\..\webmerge.bat" -f "%~dp0\conf\sprites.conf.xml" -o -png fam hires %*
+
+start "" "http://localhost:8080/fam"
+start "" "http://localhost:8080/hires"
+
+call "%~dp0\..\webmerge.bat" -f "%~dp0\conf\sprites.conf.xml" -o -png fam hires %* --webserver
+
+REM call "%~dp0\..\webmerge.bat" -f "%~dp0\conf\sprites.conf.xml" -o -png fam hires %* --watchdog --dbg
