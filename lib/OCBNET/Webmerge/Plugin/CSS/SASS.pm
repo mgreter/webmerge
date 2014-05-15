1462510b9db1081df86d294641cd0658668d6c89
Marcel Greter <marcel.greter@ocbnet.ch>
2014-05-16 01:32:45 +0200

    Fixes ruby sass processor
    Adds better error message to debug scss syntax errors

--- lib/OCBNET/Webmerge/Plugin/CSS/SASS.pm
+++ lib/OCBNET/Webmerge/Plugin/CSS/SASS.pm
@@ -18,7 +18,7 @@ my $ns = 'css::sass';
 ################################################################################
 use IPC::Run3 qw(run3);
 ################################################################################
-my $footer = "/* roby sass base: url(%s) */\n\n";
+my $header = "/* ruby sass base: url(%s) */\n\n";
 ################################################################################
 
 sub process
@@ -43,15 +43,50 @@ sub process
 	# also changes urls in comments (needed for the spriteset feature)
 	# ${$data} =~ s/$re_url/wrapURL(exportURI($+{url}, $directory))/egm;
 
+	# set encoding options
+	my $options = {
+		'binmode_stdin' => ':encoding(utf8)',
+		'binmode_stdout' => ':encoding(utf8)',
+		'binmode_stderr' => ':encoding(utf8)'
+	};
+
 	# now call run3 to compile the javascript code
-	my $rv = run3($command, $data, \ my $compiled, \ my $err);
+	my $rv = run3($command, $data, \ my $compiled, \ my $err, $options);
+
+	# fix input output handles
+	OCBNET::Webmerge::fixIOenc;
 
 	# print content to console if we have errors
 	# this should only ever print error messages
 	print $compiled if $err || $? || $rv != 1;
 
-	# check if there was any error given by closure compiler
-	die "sass compiler had an error, aborting\n$err", "\n" if $err;
+	# check if we have an error on some line
+	# this is not optimal, but it is usefull
+	# if it works (better than nothing at all)
+	if ($err)
+	{
+		# normalize the error message a little
+		$err =~ s/\: expected/\:\nexpected/g;
+		# add main error message to the final result
+		# maybe add a more elaborate error handling later
+		chomp $err; my @errors = (lcfirst $err);
+		# add a generic error header to explain it better
+		unshift @errors, 'sass compiler reported an error';
+		# check if we have a code location
+		if($err =~ m/line\s+(\d+)/)
+		{
+			# split existing code into lines
+			my @lines = split /\n/, ${$data};
+			# extract the lines that are interesting
+			for (my $i = $1 - 7; $i < $1 + 3; $i++)
+			{
+				my $line = substr $lines[$i - 1], 0, 60;
+				push @errors, sprintf "%s | %s", $i, $line;
+			}
+		}
+		# throw actual errors
+		die join "\n", @errors, "\n";
+	}
 
 	# test if ipc run3 returned success
 	die "sass compiler exited unexpectedly (code $?)", "\n" if $?;
@@ -65,10 +100,10 @@ sub process
 	# $compiled =~ s/$re_url/wrapURL(importURI($+{url}, $directory, $config))/egm;
 
 	# add an indicator about the processor (put compiled code)
-	${$data} = sprintf($footer, $file->path ) . ${$data};
+	${$data} = sprintf($header, $file->path) . $compiled;
 
 	# return reference
-	return $data;
+	return \ $compiled;
 
 }
 # EO process
