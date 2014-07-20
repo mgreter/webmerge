ea7f76105384d54a3bc0565eb6b2ae71bdfd5f47
Marcel Greter <marcel.greter@ocbnet.ch>
2014-07-20 16:35:36 +0200

    Finish first msi installer creation
    Needs wix toolset (tested with v3.8)

--- pack/win/app/07-pack-64.bat
+++ pack/win/app/07-pack-64.bat
@@ -12,13 +12,31 @@ pushd 64
 
 pp -B -o webmerge.exe ^
 -I ../../../../lib ^
+-M File::chdir ^
 -M CSS::Sass ^
 -M Encode::CN ^
 -M Encode::JP ^
 -M Encode::KR ^
 -M Encode::TW ^
 -M Encode::Byte ^
+-M Encode::Unicode ^
 -M XML::LibXML::SAX ^
+-M OCBNET::Webmerge::Plugin::JS::Minify ^
+-M OCBNET::Webmerge::Plugin::JS::Compile ^
+-M OCBNET::Webmerge::Plugin::JS::License ^
+-M OCBNET::Webmerge::Plugin::JS::NodeMinify ^
+-M OCBNET::Webmerge::Plugin::CSS::Minify ^
+-M OCBNET::Webmerge::Plugin::CSS::Compile ^
+-M OCBNET::Webmerge::Plugin::CSS::SASS ^
+-M OCBNET::Webmerge::Plugin::CSS::SCSS ^
+-M OCBNET::Webmerge::Plugin::CSS::Lint ^
+-M OCBNET::Webmerge::Plugin::CSS::License ^
+-M OCBNET::Webmerge::Plugin::CSS::InlineData ^
+-M OCBNET::Webmerge::Plugin::CSS::Spritesets ^
+-M OCBNET::Webmerge::XML::File::Input::JS ^
+-M OCBNET::Webmerge::XML::File::Output::JS ^
+-M OCBNET::Webmerge::XML::File::Input::CSS ^
+-M OCBNET::Webmerge::XML::File::Output::CSS ^
 -l "%PERLPATH%/c/bin/zlib1__.dll" ^
 -l "%PERLPATH%/c/bin/liblzma-5__.dll" ^
 -l "%PERLPATH%/c/bin/libxml2-2__.dll" ^
--- pack/win/app/82-wix-prepare-32.bat
+++ pack/win/app/82-wix-prepare-32.bat
@@ -7,4 +7,21 @@ xcopy ..\..\..\..\conf conf /i /Y /Q /E
 xcopy ..\..\..\..\vendor vendor /i /Y /Q /E
 xcopy ..\..\..\..\example example /i /Y /Q /E
 
+cd vendor
+cd google
+cd closure
+
+call update
+
+cd ..
+cd ..
+
+cd yahoo
+cd yui
+
+call update
+
+cd ..
+cd ..
+
 cd ..
